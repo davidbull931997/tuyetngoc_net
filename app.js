9958d6a6b667847cff3c472c480d34645cf7134b
Lâm Phan Ngọc <davidbull931997@gmail.com>
2017-07-05 18:19:56 +0700

    force SSL heroku

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var force_ssl_heroku = require('force-ssl-heroku');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -21,6 +22,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(force_ssl_heroku);
 
 app.use('/', index);
 app.use('/users', users);
